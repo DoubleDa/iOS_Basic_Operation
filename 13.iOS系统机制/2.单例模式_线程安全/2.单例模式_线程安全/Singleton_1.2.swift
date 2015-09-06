9b0bc7aa18ede1be03eb769bee1ff0a4366fdf13
yyn835314557 <835314557@qq.com>
2015-09-06 09:08:34 +0800

    modify property

---
+++ 13.iOS系统机制/2.单例模式_线程安全/2.单例模式_线程安全/Singleton_1.2.swift
@@ -0,0 +1,15 @@
+//
+//  Singleton_1.2.swift
+//  2.单例模式_线程安全
+//
+//  Created by 游义男 on 15/9/6.
+//  Copyright © 2015年 游义男. All rights reserved.
+//
+
+import Foundation
+
+class singleton {
+    static let instance:Singleton = Singleton()
+}
+
+
