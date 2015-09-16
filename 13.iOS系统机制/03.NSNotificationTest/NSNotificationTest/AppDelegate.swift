40205fdfe583c6c1931c6825f3c3c82ea140678b
yyn835314557 <835314557@qq.com>
2015-09-16 09:54:41 +0800

    NSNotificationDemo

--- 13.iOS系统机制/03.NSNotificationTest/NSNotificationTest/AppDelegate.swift
+++ 13.iOS系统机制/03.NSNotificationTest/NSNotificationTest/AppDelegate.swift
@@ -13,11 +13,126 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(application: UIApplication, handleActionWithIdentifier identifier: String?, forLocalNotification notification: UILocalNotification, completionHandler: () -> Void) {
+        if identifier == "FIRST_ACTION" {
+            
+            NSNotificationCenter.defaultCenter().postNotificationName("actionOne", object: nil)
+            
+        }
+        
+        else if identifier == "SECOND_ACTION" {
+            
+            NSNotificationCenter.defaultCenter().postNotificationName("actionTwo", object: nil)
+            
+        }
+        
+        completionHandler()
+    }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+//        let types = UIUserNotificationType.Alert    // //设置Notification 的类型
+//        let mySettings = UIUserNotificationSettings(forTypes: types, categories: nil)   //设置Notification的设置项，其中categories参数用来设置Notification的类别
+//        UIApplication.sharedApplication().registerUserNotificationSettings(mySettings)  //注册UserNotification
+        
+        //define Actions
+        
+        let firstAction: UIMutableUserNotificationAction = UIMutableUserNotificationAction();
+        
+        // The unique identifier for this action
+        
+        firstAction.identifier = "FIRST_ACTION";
+        
+        // The localized title to display for this action
+        
+        firstAction.title = "First Action";
+        
+        //define action’s activationMode, // How the application should be activated in response to the action
+        
+        firstAction.activationMode = UIUserNotificationActivationMode.Background// 当点击的时候不启动程序，在后台处理
+        
+        //define action’s destructive // Whether this action should be indicated as destructive when displayed.
+        
+        firstAction.destructive = true
+        
+        //define authentication // Whether this action is secure and should require unlocking before being performed. If the activation mode is UIUserNotificationActivationModeForeground, then the action is considered secure and this property is ignored.
+        
+        firstAction.authenticationRequired = false//不需要用户解锁手机即可以处理该Notification
+        
+        
+        
+        var secondAction:UIMutableUserNotificationAction = UIMutableUserNotificationAction()
+        
+        secondAction.identifier = "SECOND_ACTION";
+        
+        secondAction.title = "Second Action";
+        
+        secondAction.activationMode = UIUserNotificationActivationMode.Foreground
+        
+        secondAction.destructive = false
+        
+        secondAction.authenticationRequired = false
+        
+        
+        
+        var thirdAction:UIMutableUserNotificationAction = UIMutableUserNotificationAction()
+        
+        thirdAction.identifier = "THIRD_ACTION";
+        
+        thirdAction.title = "Third Action";
+        
+        thirdAction.activationMode = UIUserNotificationActivationMode.Background
+        
+        thirdAction.destructive = false
+        
+        thirdAction.authenticationRequired = false
+        
+        
+        
+        //Category
+        
+        let firstCategory: UIMutableUserNotificationCategory = UIMutableUserNotificationCategory()
+        
+        firstCategory.identifier = "FIRST_CATEGORY";
+        
+        
+        
+        let defaultActions:NSArray = [firstAction, secondAction, thirdAction];
+        
+        let minimalActions:NSArray = [firstAction, secondAction];
+        
+        
+        
+        // Sets the UIUserNotificationActions in the order to be displayed for the specified context
+        
+        firstCategory.setActions(defaultActions as? [UIUserNotificationAction], forContext: UIUserNotificationActionContext.Default);// // the default context of a notification action
+        
+        firstCategory.setActions(minimalActions as? [UIUserNotificationAction], forContext: UIUserNotificationActionContext.Minimal);//Minimal // the context of a notification action when space is limited
+        
+        
+        
+        let categories: NSSet = NSSet(objects: firstCategory)
+        
+        
+        
+        
+        
+        //设置Notification 的类型
+        
+        let types:UIUserNotificationType = UIUserNotificationType.Alert
+        //设置Notification的设置项，其中categories参数用来设置Notification的类别
+        
+        let mySettings: UIUserNotificationSettings = UIUserNotificationSettings(forTypes: types, categories: categories as? Set<UIUserNotificationCategory>);
+        
+        //注册UserNotification
+        
+        UIApplication.sharedApplication().registerUserNotificationSettings(mySettings)
+        
         return true
+        
     }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- 13.iOS系统机制/03.NSNotificationTest/NSNotificationTest/ViewController.swift
+++ 13.iOS系统机制/03.NSNotificationTest/NSNotificationTest/ViewController.swift
@@ -13,42 +13,120 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
         
-    }
-    override func viewDidAppear(animated: Bool) {
-        // 创建静态的通知名字串
-        let notificationName :String = "PostArticleSuccessNotification"
+        //define notification center
         
-        //然后，使用通知中心来Post通知 （2种方式, 方法） pattern1
-        NSNotificationCenter.defaultCenter().postNotificationName(notificationName, object: "Object", userInfo: ["statu":"success"])
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "TestShape:", name: "actionOne", object: nil)
         
-        // pattern2
-        let notification = NSNotification(name: notificationName, object: "Object", userInfo: ["status" : "success"])
-        NSNotificationCenter.defaultCenter().postNotification(notification)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"TestMessage:", name: "actionTwo", object: nil)
         
-        /**
-        *  使某一个对象成为监听者
-        */
-        // pattern1
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "NotificationAction:", name: notificationName, object: nil)
-        //pattern2
-        NSNotificationCenter.defaultCenter().addObserverForName(notificationName, object: self, queue: NSOperationQueue.mainQueue()) { (note) -> Void in
-            print(note.name)
-            print(note.object)
-            print(note.userInfo)
-        }
+        //定义一个触发Notification的程序
+        let dateComp: NSDateComponents  = NSDateComponents()
         
-        // 是根据NotificationName来移除指定的一个通知监听者的 ,若没有参数则全部移除
-//        NSNotificationCenter.defaultCenter().removeObserver(self, name: notificationName, object: nil)
-    }
-
-    func NotificationAction(note:NSNotification){
-        print(note.name)
-        print(note.object)
-        print(note.userInfo)
+        dateComp.year = 2014
+        
+        dateComp.month = 09
+        
+        dateComp.day = 09
+        
+        dateComp.hour = 11
+        
+        dateComp.minute = 11
+        
+        dateComp.timeZone = NSTimeZone.systemTimeZone()
+        
+        
+        
+        let calendar: NSCalendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)!
+        
+        let date: NSDate = calendar.dateFromComponents(dateComp)!
+        
+        
+        
+        
+        
+        //define Location notification
+        
+        let notification: UILocalNotification = UILocalNotification()
+        
+        notification.category = "FIRST_CATEGORY";
+        
+        notification.alertBody = "This is a notification"
+        
+        notification.fireDate = date
+        
+        
+        
+        //fire notification
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        
     }
     
+    
+    
+    func TestShape(notification: NSNotification) {
+        
+        var view = UIView(frame: CGRectMake(100,100,100,100));
+        
+        view.backgroundColor = UIColor.blackColor()
+        
+        self.view.addSubview(view)
+        
+    }
+    
+    
+    
+    func TestMessage(notification: NSNotification) {
+        
+        var message: UIAlertController = UIAlertController(title: "Notification Message", message: "Hello, this is an alert message", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        message.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        
+        
+        
+        self.presentViewController(message, animated: true, completion: nil)
+        
+    }
+    
+
+        
+    
+//    override func viewDidAppear(animated: Bool) {
+//        // 创建静态的通知名字串
+//        let notificationName :String = "PostArticleSuccessNotification"
+//        
+//        //然后，使用通知中心来Post通知 （2种方式, 方法） pattern1
+//        NSNotificationCenter.defaultCenter().postNotificationName(notificationName, object: "Object", userInfo: ["statu":"success"])
+//        
+//        // pattern2
+//        let notification = NSNotification(name: notificationName, object: "Object", userInfo: ["status" : "success"])
+//        NSNotificationCenter.defaultCenter().postNotification(notification)
+//        
+//        /**
+//        *  使某一个对象成为监听者
+//        */
+//        // pattern1
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: "NotificationAction:", name: notificationName, object: nil)
+//        //pattern2
+//        NSNotificationCenter.defaultCenter().addObserverForName(notificationName, object: self, queue: NSOperationQueue.mainQueue()) { (note) -> Void in
+//            print(note.name)
+//            print(note.object)
+//            print(note.userInfo)
+//        }
+//        
+//        // 是根据NotificationName来移除指定的一个通知监听者的 ,若没有参数则全部移除
+////        NSNotificationCenter.defaultCenter().removeObserver(self, name: notificationName, object: nil)
+//    }
+
+    
+//
+//    func NotificationAction(note:NSNotification){
+//        print(note.name)
+//        print(note.object)
+//        print(note.userInfo)
+//    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
