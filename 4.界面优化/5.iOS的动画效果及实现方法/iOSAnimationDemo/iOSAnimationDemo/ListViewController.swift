c0374b2408cb89a4a66bb7d43d6640ae50c7901a
yyn835314557 <835314557@qq.com>
2015-11-24 14:56:00 +0800

    modify 界面优化防止卡顿

--- 4.界面优化/5.iOS的动画效果及实现方法/iOSAnimationDemo/iOSAnimationDemo/ListViewController.swift
+++ 4.界面优化/5.iOS的动画效果及实现方法/iOSAnimationDemo/iOSAnimationDemo/ListViewController.swift
@@ -12,6 +12,7 @@ class ListViewController: UIViewController {
 
     var redSquareView:UIView = UIView(frame: CGRect(x: 20, y: 100, width: 50, height: 50))
     var blueSqureView:UIView = UIView(frame: CGRect(x: 50, y: 100, width: 50, height: 50))
+    var yellowSquareView:UIView = UIView(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
 
     
     override func viewDidLoad() {
@@ -20,14 +21,22 @@ class ListViewController: UIViewController {
         // Do any additional setup after loading the view.
         redSquareView.backgroundColor = UIColor.redColor().colorWithAlphaComponent(0.75)
         blueSqureView.backgroundColor = UIColor.greenColor().colorWithAlphaComponent(0.75)
+        yellowSquareView.backgroundColor = UIColor.yellowColor().colorWithAlphaComponent(0.75)
         self.view.addSubview(redSquareView)
         self.view.addSubview(blueSqureView)
+        self.view.addSubview(yellowSquareView)
 
     }
     override func viewDidAppear(animated: Bool) {
         UIView.animateWithDuration(0.5) { () -> Void in
             self.redSquareView.backgroundColor = UIColor.magentaColor().colorWithAlphaComponent(0.75)
             self.blueSqureView.center.x = self.view.frame.size.width - self.blueSqureView.bounds.width
+            self.yellowSquareView.transform.a = 2
+//            self.yellowSquareView.transform.b = 2
+//            self.yellowSquareView.transform.c = 2
+//            self.yellowSquareView.transform.d = 2
+//            self.yellowSquareView.transform.tx = 2
+//            self.yellowSquareView.transform.ty = 2
         }
     }
 
